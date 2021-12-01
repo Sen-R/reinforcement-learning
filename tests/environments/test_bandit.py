787b24d2ece40dd730a0f447ee754f7b077abc1d
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2021-12-01 10:07:24 +0000

    refactor: moved bandit (and tests) to environments subpackage

--- tests/environments/test_bandit.py
+++ tests/environments/test_bandit.py
@@ -1,6 +1,6 @@
 import unittest
 from numpy.testing import assert_almost_equal
-from rlbook.bandit import MultiArmBandit
+from rlbook.environments.bandit import MultiArmBandit
 
 
 class TestBandit(unittest.TestCase):
