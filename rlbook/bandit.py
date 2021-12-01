a9669cbdb1661743f63b9d0e1d78b3eae3c774b1
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2021-12-01 09:59:22 +0000

    feat: initial implementation of multi-armed bandit

---
+++ rlbook/bandit.py
@@ -0,0 +1,38 @@
+"""Implementation of a multi-armed bandit.
+
+This module provides an implementation of a multi-armed bandit environment.
+"""
+
+
+import numpy as np
+
+
+class MultiArmBandit:
+    """A multi-armed bandit.
+
+    Class representing a multi-armed bandit. Pulling one of its levers
+    by calling `act` yields a scalar reward. Rewards for each lever
+    are normally distributed with parameters defined when the bandit
+    is initialised.
+
+    Args:
+      means: Sequence of mean rewards for each lever
+      sigma: Sequence of reward standard deviations for each lever
+      random_state: `None`, `int`, `Generator` etc to initialise RNG.
+    """
+
+    def __init__(self, means, sigmas, random_state=None):
+        self.means = means
+        self.sigmas = sigmas
+        self._rng = np.random.default_rng(random_state)
+
+    @property
+    def k(self):
+        """Returns the number of levers for this bandit."""
+        return len(self.means)
+
+    def act(self, lever):
+        """Returns reward for pulling lever `lever`."""
+        return self._rng.normal(
+            loc=self.means[lever], scale=self.sigmas[lever]
+        )
---
+++ tests/test_bandit.py
@@ -0,0 +1,23 @@
+import unittest
+from numpy.testing import assert_almost_equal
+from rlbook.bandit import MultiArmBandit
+
+
+class TestBandit(unittest.TestCase):
+    def test_bandit_setup(self):
+        means = [1.0, 2.0, 3.0]
+        sigmas = [1.0, 1.5, 0.5]
+        b = MultiArmBandit(means, sigmas)
+        self.assertEqual(means, b.means)
+        self.assertEqual(sigmas, b.sigmas)
+
+    def test_bandit_k(self):
+        b = MultiArmBandit([1.0], [1.0])
+        self.assertEqual(b.k, len(b.means))
+
+    def test_bandit_act(self):
+        b = MultiArmBandit([0.0, -0.5], [1.0, 2.0], random_state=42)
+        actions = [1, 0, 1]
+        rewards = [b.act(a) for a in actions]
+        expected = [0.1094341595, -1.0399841062, 1.000902391]
+        assert_almost_equal(rewards, expected)
