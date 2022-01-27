e172971920a9037e8a34d57f40153a19f028c1d0
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2022-01-27 21:20:12 +0000

    feat: added UCBRewardAveragingAgent and associated tests

--- rl/agents.py
+++ rl/agents.py
@@ -2,7 +2,7 @@ from typing import Optional, Sequence
 from .agent import Agent
 from .custom_types import LearningRateSchedule
 from .policies.value_learning import RewardAveragingPolicy
-from .policies.action_selection_strategy import EpsilonGreedy
+from .policies.action_selection_strategy import EpsilonGreedy, UCB
 
 
 class EpsilonGreedyRewardAveragingAgent(Agent):
@@ -33,6 +33,39 @@ class EpsilonGreedyRewardAveragingAgent(Agent):
         policy = RewardAveragingPolicy(
             n_actions,
             action_selection_strategy=EpsilonGreedy(epsilon, random_state),
+            learning_rate_schedule=learning_rate_schedule,
+            initial_action_values=initial_action_values,
+        )
+        super().__init__(policy)
+
+
+class UCBRewardAveragingAgent(Agent):
+    """Agent that estimates values by averaging immediate rewards and selects
+    actions using Upper Confidence Bound selection.
+
+    See Chapter 2 of Sutton, Barto (2018) for details.
+
+    Args:
+      c: `c` parameter for Upper Confidence Bound method
+      n_actions: size of action space
+      learning_rate_schedule: function mapping action count to soft update
+        parameter `alpha`. Can be one of the objects provided in
+        `rl.learningrate`
+      initial_action_values: initial estimates of the value of each action,
+        by default set to zero for all actions
+    """
+
+    def __init__(
+        self,
+        c: float,
+        n_actions: int,
+        *,
+        learning_rate_schedule: LearningRateSchedule = None,
+        initial_action_values: Optional[Sequence[float]] = None,
+    ):
+        policy = RewardAveragingPolicy(
+            n_actions,
+            action_selection_strategy=UCB(c),
             learning_rate_schedule=learning_rate_schedule,
             initial_action_values=initial_action_values,
         )
--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,7 +1,10 @@
 import numpy as np
 from rl.policies.value_learning import RewardAveragingPolicy
-from rl.policies.action_selection_strategy import EpsilonGreedy
-from rl.agents import EpsilonGreedyRewardAveragingAgent
+from rl.policies.action_selection_strategy import EpsilonGreedy, UCB
+from rl.agents import (
+    EpsilonGreedyRewardAveragingAgent,
+    UCBRewardAveragingAgent,
+)
 from rl.learningrate import SampleAverageLearningRate
 
 
@@ -42,6 +45,44 @@ class TestEpsilonGreedyRewardAveragingAgent:
         epsilon = 0.1
         n_actions = 2
         agent = EpsilonGreedyRewardAveragingAgent(epsilon, n_actions)
+        policy = agent.policy
+        assert isinstance(policy, RewardAveragingPolicy)
+        assert isinstance(policy.alpha, SampleAverageLearningRate)
+        assert policy.Q == [0.0] * 2
+
+
+class TestUCBRewardAveragingAgent:
+    def test_init(self) -> None:
+        # Create agent
+        c = 2
+        n_actions = 2
+
+        def learning_rate_schedule(_):
+            return 0.1
+
+        initial_action_values = [1.0, 2.0]
+
+        agent = UCBRewardAveragingAgent(
+            c,
+            n_actions,
+            learning_rate_schedule=learning_rate_schedule,
+            initial_action_values=initial_action_values,
+        )
+
+        # Check created agent is of right type and contains right fields
+        policy = agent.policy
+        assert isinstance(policy, RewardAveragingPolicy)
+        assert isinstance(policy.action_selection_strategy, UCB)
+        assert policy.action_selection_strategy.c == c
+        assert len(policy.Q) == n_actions
+        assert policy.alpha == learning_rate_schedule
+        assert policy.Q == initial_action_values
+
+    def test_defaults(self) -> None:
+        # Create agent
+        c = 2
+        n_actions = 2
+        agent = UCBRewardAveragingAgent(c, n_actions)
         policy = agent.policy
         assert isinstance(policy, RewardAveragingPolicy)
         assert isinstance(policy.alpha, SampleAverageLearningRate)
