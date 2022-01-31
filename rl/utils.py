f983d0803a759e2df487a80dcc27ded3e2ca855c
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2022-01-31 19:59:09 +0000

    feat: added UpdatableParameter, SoftUpdateParameter and FixedParameter classes and tests

--- rl/utils.py
+++ rl/utils.py
@@ -1,4 +1,10 @@
 """Utilities module for rl package."""
+from typing import Callable, TypeVar, Generic, Optional, Union
+from abc import ABC, abstractmethod
+from .learningrate import SampleAverageLearningRate, ConstantLearningRate
+
+
+Param = TypeVar("Param")
 
 
 def soft_update(current, target, alpha: float):
@@ -12,3 +18,72 @@ def soft_update(current, target, alpha: float):
     In the limit `alpha == 1` corresponds to replacing `current` by `target`.
     """
     return (1.0 - alpha) * current + alpha * target
+
+
+class UpdatableParameter(ABC, Generic[Param]):
+    """ABC for parameters that update (typically using soft-update rule) based
+    on observations."""
+
+    @abstractmethod
+    def __call__(self) -> Param:
+        """Returns the current value of the parameter."""
+        pass
+
+    @abstractmethod
+    def update(self, value: Param) -> None:
+        """Uses new observation `value` to update the parameter value."""
+        pass
+
+
+class SoftUpdatedParameter(UpdatableParameter):
+    """Object to hold parameter and functionality to soft update it.
+
+    Args:
+      initial_value: initial value of the parameter
+      learning_rate_schedule: callable determining alpha(n)
+    """
+
+    def __init__(
+        self,
+        initial_value: Param,
+        learning_rate_schedule: Optional[
+            Union[float, Callable[[int], float]]
+        ] = None,
+    ):
+        self._value = initial_value
+        if callable(learning_rate_schedule):
+            self._learning_rate_schedule = learning_rate_schedule
+        elif learning_rate_schedule is None:
+            self._learning_rate_schedule = SampleAverageLearningRate()
+        else:
+            self._learning_rate_schedule = ConstantLearningRate(
+                float(learning_rate_schedule)
+            )
+        self._n = 0
+
+    def __call__(self) -> Param:
+        return self._value
+
+    def update(self, value: Param) -> None:
+        self._value = soft_update(
+            self._value, value, self._learning_rate_schedule(self._n)
+        )
+        self._n += 1
+
+
+class FixedParameter(UpdatableParameter):
+    """Special case of SoftUpdatedParameter with alpha=0.
+
+    I.e. the parameter remains its initial value even after calling
+    `update` method.
+    """
+
+    def __init__(self, initial_value: Param):
+        self._value = initial_value
+
+    def __call__(self) -> Param:
+        return self._value
+
+    def update(self, value: Param) -> None:
+        # No-op for FixedParameter
+        pass
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,10 @@
-from rl.utils import soft_update
+from rl.utils import (
+    soft_update,
+    SoftUpdatedParameter,
+    FixedParameter,
+    UpdatableParameter,
+)
+from rl.learningrate import SampleAverageLearningRate
 
 
 class TestSoftUpdate:
@@ -8,3 +14,63 @@ class TestSoftUpdate:
         target = 1.5
         expected = 1.1  # (1 - alpha) * current + alpha * target
         assert soft_update(current, target, alpha) == expected
+
+
+class TestSoftUpdatedParameter:
+    def test_functionality(self) -> None:
+        # Set up parameter
+        lr_schedule = SampleAverageLearningRate()
+        param = SoftUpdatedParameter(
+            initial_value=1.0, learning_rate_schedule=lr_schedule
+        )
+
+        # Test param is a subclass of UpdatableParameter
+        assert isinstance(param, UpdatableParameter)
+
+        # Test initial value is correct
+        assert param() == 1.0
+
+        # Test value is correctly updated after first iteration
+        param.update(2.0)
+        assert param() == 2.0
+
+        # Test value is correctly updated after second iteration (and
+        # implicitly that alpha has been updated to facilitate this)
+        param.update(3.0)
+        assert param() == 2.5
+
+    def test_providing_number_as_learning_rate(self) -> None:
+        """When a number is provided as the learning rate, this should
+        have been converted into a ConstantLearningRate schedule."""
+        param = SoftUpdatedParameter(0.0, 0.1)
+        param.update(1.0)
+        assert param() == 0.1
+        param.update(0.5)
+        assert param() == 0.1 * 0.5 + 0.9 * 0.1
+
+    def test_providing_default_learning_rate_schedule_is_sample_averaging(
+        self,
+    ) -> None:
+        param = SoftUpdatedParameter(0.0)
+        param.update(1.0)
+        assert param() == 1.0
+        param.update(-1.0)
+        assert param() == 0.0
+        param.update(3.0)
+        assert param() == 1.0
+
+
+class TestFixedParameter:
+    def test_functionality(self):
+        param = FixedParameter(1.0)
+
+        # Should be a subclass of UpdatableParameter
+        assert isinstance(param, UpdatableParameter)
+
+        # Should return the initial value no matter whether this has
+        # been "updated"
+        assert param() == 1.0
+        param.update(2.0)
+        assert param() == 1.0
+        param.update(3.0)
+        assert param() == 1.0
