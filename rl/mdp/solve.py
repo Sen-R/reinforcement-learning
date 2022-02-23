b32b0cf0227a874751efb680f12dc3f5e76df340
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2022-02-23 10:06:30 +0000

    feat: added exact_optimum_state_values function for solving Bellman optimality equation

--- rl/mdp/solve.py
+++ rl/mdp/solve.py
@@ -1,5 +1,6 @@
-from typing import Callable, Dict
+from typing import Callable, Dict, Optional
 import numpy as np
+from scipy import optimize  # type: ignore
 from ._types import State, Action
 from .base import FiniteMDP
 
@@ -9,6 +10,50 @@ def exact_state_values(
     gamma: float,
     pi: Callable[[Action, State], float],
 ) -> Dict[State, float]:
+    """Returns state values for given policy.
+
+    This function directly solves the (linear) Bellman equation to calculate
+    the state value function for the policy represented by `pi`.
+
+    Args:
+      mdp: MDP for which state values are being calculated
+      gamma: discount factor
+      pi: conditional probabilities for actions given states, encoding the
+        policy to be evaluated
+
+    Returns:
+      `dict` mapping states to state values
+    """
     A, b = mdp.backup_policy_values_operator(gamma, pi)
     v = np.linalg.solve(np.eye(len(mdp.states)) - A, b)
     return {mdp.i2s(idx): value for idx, value in enumerate(v)}
+
+
+def exact_optimum_state_values(
+    mdp: FiniteMDP[Action, State], gamma: float, tol: Optional[float] = None
+) -> Dict[State, float]:
+    """Returns state values for an optimal policy for the given MDP.
+
+    This function uses a non-linear solver to directly solve the Bellman
+    optimality equation, to calculate state values for an optimal policy.
+
+    Args:
+      mdp: MDP for which optimal state values are being calculated
+      gamma: discount factor
+
+    Returns:
+      `dict` mapping states to state values under an optimal policy
+    """
+    initial_guess = np.zeros(len(mdp.states))
+    opt_result = optimize.root(
+        lambda v_star: v_star - mdp.backup_optimal_values(v_star, gamma),
+        x0=initial_guess,
+        tol=tol,
+    )
+
+    if not opt_result.success:
+        raise optimize.OptimizeWarning(
+            "Root finding failed to find a solution", opt_result
+        )
+
+    return {mdp.i2s(idx): val for idx, val in enumerate(opt_result.x)}
--- tests/mdp/test_solve.py
+++ tests/mdp/test_solve.py
@@ -1,5 +1,5 @@
 from rl.mdp import FiniteMDP
-from rl.mdp.solve import exact_state_values
+from rl.mdp.solve import exact_state_values, exact_optimum_state_values
 
 
 class TestExactStateValues:
@@ -12,3 +12,15 @@ class TestExactStateValues:
         assert round(v[(3, 1)], 1) == -0.4
         assert round(v[(4, 3)], 1) == -1.4
         assert round(v[(0, 1)], 1) == 8.8
+
+
+class TestExactOptimumStateValues:
+    def test_optimum_state_values_for_sutton_barto_gridworld(
+        self, gridworld: FiniteMDP
+    ) -> None:
+        """Compares outputs of `exact_optimum_state_values` against results
+        given in the textbook where this gridworld is introduced."""
+        v_star = exact_optimum_state_values(gridworld, gamma=0.9)
+        assert round(v_star[(3, 1)], 1) == 17.8
+        assert round(v_star[(4, 3)], 1) == 13.0
+        assert round(v_star[(0, 1)], 1) == 24.4
